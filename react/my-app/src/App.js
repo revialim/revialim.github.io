3da65275801805f70c41d443b20e9ce3fe4f0a99
Lilian Hung <l.hung.m@gmail.com>
2017-11-20 07:20:06 +0800

    somehow getting going

---
+++ react/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p>
+          asdf...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Main;
