58894915edc3970e370b3afc4084303493775847
Lilian Hung <l.hung.m@gmail.com>
2017-12-06 08:16:17 +0800

    used approximate function to calculate days on earth(timestamp difference)

--- days-on-this-earth.js
+++ days-on-this-earth.js
@@ -1,16 +1,37 @@
-
-//var birthday = new Date("12/15/2010");
-
-
-//var timeDiff = Math.abs(birthday.getTime() - today.getTime());
-//var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-
 function calcDaysOnEarth() {
   var today = new Date();
   var birthday = document.getElementById("birthday-field");
-  console.log(birthday.innerHTML);
-  console.log("today:" + today.getDate);
-  today = new Date();
+  var birthday_date = new Date(birthday.value);
+  var earthday = document.getElementById("earthday-field");
+  var days;
+  var dayString;
+  //console.log(birthday.value +" birthday date: " + birthday_date.toDateString());
+  if(birthday_date.getTime() > today.getTime()){
+    earthday.innerHTML = "Dear time traveller, your birthday lies in the future :O";
+  } else {
+    days = dayDiffByTimestamp(today, birthday_date);
+    dayString = (days) => {
+      if(days.toString().slice(-1) == "1"){
+        return "st";
+      } else if(days.toString().slice(-1) == "2"){
+        return "nd";
+      } else if(days.toString().slice(-1) == "3"){
+        return "rd";
+      } else {
+        return "th";
+      }
+    };
+    earthday.innerHTML = "Today is your " + days + dayString(days) + " earthday!";
+  }
+  //console.log("dayString: "+ today.toDateString());
+  //console.log("today: " + (today.getDate() < 10 ? "0" + today.getDate() : today.getDate())+ "." + (today.getMonth()+1) + "." + today.getFullYear() + " at " + today.getHours() + ":" + today.getMinutes() + ":" + today.getMilliseconds() );
+}
   
-  console.log("later today:" + today.getDate().day);
+function isLeapYear(year){
+  
+}
+  
+function dayDiffByTimestamp(date1, date2){
+  var timediff = Math.abs(date2.getTime() - date1.getTime()); 
+  return Math.ceil(timediff / (1000 * 3600 * 24)); //1000 * 60 * 60 * 24 is approximate number of milliseconds per day
 }
