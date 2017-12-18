43039b3ebe3a74b2f4a57f593d86cf9914eb8818
Lilian Hung <l.hung.m@gmail.com>
2017-12-18 17:16:32 +0800

    there's a moving circle, now, i am tired

--- jump-game/jump-and-more.js
+++ jump-game/jump-and-more.js
@@ -1,16 +1,76 @@
-var CANVAS_HEIGHT = 300;
-var CANVAS_WIDTH = 400;
+var canvas;
+var ctx; 
+var ballRadius;
+var x;
+var y;
+var dx;
+var dy;
+
+function init() {
+  canvas = document.getElementById("jump");
+  ctx = canvas.getContext("2d");
+  ballRadius = 10;
+  x = canvas.width/2;
+  y = canvas.height-30;
+  dx = 2;
+  dy = -2; 
+  
+  drawBall();
+}
+
+function drawBall(x,y) {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  console.log("in draw, x:" + x + ", y:" + y + "; dx: " + dx + ", dy: " + dy);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall(x,y);
+  
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+      dx = -dx;
+  }
+  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+      dy = -dy;
+  }
+  
+  x = x + dx;
+  y = y + dy;
+
+  console.log("in draw2, x:" + x + ", y:" + y + "; dx: " + dx + ", dy: " + dy);
+  window.requestAnimationFrame(draw);
+}
+
+function main() {
+  init();
+  draw();
+  ////setInterval(draw, 10);
+  console.log("test");
+  
+  //window.requestAnimationFrame(draw);
+  //draw(x,y);
+  //window.addEventListener("keydown", checkKeyPressed, false);
+  
+}
+
+/*
+//var CANVAS_HEIGHT = 300;
+//var CANVAS_WIDTH = 400;
 var JUMP_GAME = {
-  player: new Image(),
+  playerImg: new Image(),
   stopMain: null
 }
+*/
 
 /**
  * REFERENCES
  * https://developer.mozilla.org/en-US/docs/Games/Anatomy
  *
- *
- */
+
 
 ;(function () {
   function main( tFrame ) {
@@ -26,51 +86,65 @@ var JUMP_GAME = {
       numTicks = Math.floor( timeSinceTick / JUMP_GAME.tickLength );
     }
     
-    update( tFrame ); //Call your update method. In our case, we give it rAF's timestamp.
-    render();
+    queueUpdates( numTicks );
+    render( tFrame );
+    JUMP_GAME.lastRender = tFrame;
   }
   
-  main(); // Start the cycle
+  function queueUpdates( numTicks ) {
+    for(var i=0; i < numTicks; i++) {
+      JUMP_GAME.lastTick = JUMP_GAME.lastTick + JUMP_GAME.tickLength; //Now lastTick is this tick.
+      update( JUMP_GAME.lastTick );
+    }
+  }
+  
+  JUMP_GAME.lastTick = performance.now();
+  JUMP_GAME.lastRender = JUMP_GAME.lastTick; //Pretend the first draw was on first update.
+  JUMP_GAME.tickLength = 50; //Set simulation to run at 20Hz (50ms)
+  
+  //setInitialState(); //Performs whatever tasks are leftover before the mainloop must run.
+  main(performance.now()); // Start the cycle
   
 })();
+
+
 
 //to stop main loop
 //use
 //window.cancelAnimationFrame( JUMP_GAME.stopMain );
+ *
+ */
 
 
+/*
 function init(){
-  PLAYER.scr = "./jump-and-run_graphics/jump-and-run_main-character_stand.png";
+  JUMP_GAME.playerImg.scr = "./jump-and-run_graphics/jump-and-run_main-character_stand.png";
   window.requestAnimationFrame(draw);
   var ctx = document.getElementById("jump").getContext('2d');
-
+  
   
 }
 
 function draw(){
+  oneCircle(ctx, x, y);
+  dx = -2;
+  dy = -2;
   
+  x = x+dx;
+  y = y+dy;
+  
+  //collision detection
+  if(y + dy > canvas.height || y + dy < 0){
+    dy = -dy;
+  }
+  if(x + dx > canvas.width || x + dx < 0){
+    dx = -dx;
+  }
 }
 
 function drawOnCanvas() {
   var ctx = document.getElementById("jump").getContext('2d');
   oneRect(ctx, 10, 250);
-}
-
-function oneRect(canvasContext, x, y){
-  canvasContext.fillStyle = 'rgba(200, 0, 200, 0.5)';
-  canvasContext.fillRect(x, y, 50, 50); 
-}
-
-function moveRect(canvasContext, x, y){
-  canvasContext.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  oneRect(canvasContext, x, y);
-}
-
-function twoRects(canvasContext, x, y){
-  canvasContext.fillStyle = 'rgb(200, 0, 0)';
-  canvasContext.fillRect(x, y, 50, 50);
-  canvasContext.fillStyle = 'rgba(0, 0, 200, 0.5)';
-  canvasContext.fillRect(x+20, y+20, 50, 50);
 }
 
 function checkKeyPressed(e) {
@@ -89,3 +163,40 @@ function start() {
   window.addEventListener("keydown", checkKeyPressed, false);
   
 }
+*/
+
+//==========//
+//  SHAPES  //
+//==========//
+/*
+function oneCircle(canvasContext, x, y){
+  canvasContext.beginPath();
+  canvasContext.arc(x, y, 10, 0, Math.PI*2);
+  canvasContext.fillStyle = "#0087af";
+  canvasContext.fill();
+  canvasContext.closePath();  
+}
+
+function oneRect(canvasContext, x, y){
+  canvasContext.beginPath();
+  canvasContext.fillStyle = 'rgba(200, 0, 200, 0.5)';
+  canvasContext.fillRect(x, y, 50, 50); 
+  canvasContext.closePath(); 
+}
+
+function moveRect(canvasContext, x, y){
+  canvasContext.beginPath();
+  canvasContext.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  oneRect(canvasContext, x, y);
+  canvasContext.closePath(); 
+}
+
+function twoRects(canvasContext, x, y){
+  canvasContext.beginPath();
+  canvasContext.fillStyle = 'rgb(200, 0, 0)';
+  canvasContext.fillRect(x, y, 50, 50);
+  canvasContext.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  canvasContext.fillRect(x+20, y+20, 50, 50);
+  canvasContext.closePath(); 
+}
+*/
