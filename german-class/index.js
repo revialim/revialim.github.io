aeca9c10faf0eceeaf2c0d4a8d5d3e21fb847953
Lilian Hung <l.hung.m@gmail.com>
2018-01-31 10:15:55 +0800

    added missing script to index.html to have react rendering

--- german-class/index.js
+++ german-class/index.js
@@ -1,8 +1,20 @@
-//define react components here
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class TestComponent extends React.Component {
+  render() {
+    return (
+        <div className="settings">
+            <h1> asdfasdf </h1>
+            <p>
+              testtest react:)
+            </p>
+        </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <TestComponent />,
   document.getElementById('react-elem')
 );
