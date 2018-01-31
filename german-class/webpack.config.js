90d2dc3ada09a1ca012daecf5d21cfb670cd5b3d
Lilian Hung <l.hung.m@gmail.com>
2018-01-31 09:47:14 +0800

    trying to get react snippet working

---
+++ german-class/webpack.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  entry: "./index.js",
+
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".js"]
+  },
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel-loader', query: { presets: ['es2015', 'react'] } }
+    ],
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ],
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  }/*,
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },*/
+};
